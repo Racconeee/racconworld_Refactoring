6af44fa6408e5ef4fdf7ad54244deba0183a0ddc
Racconeee <raccon6614@gmail.com>
2024-11-16 16:30:42 +0900

    Log 기능 삭제

--- raccon_BE/src/main/java/racconworld/raccon/domain/log/controller/LogController.java
+++
@@ -1,8 +0,0 @@
-package racconworld.raccon.domain.log.controller;
-
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LogController {
-}
--- raccon_BE/src/main/java/racconworld/raccon/domain/log/service/LogService.java
+++
@@ -1,9 +0,0 @@
-package racconworld.raccon.domain.log.service;
-
-
-import racconworld.raccon.domain.log.entity.Log;
-
-public interface LogService {
-
-    void saveLog(Log logEntity);
-}
